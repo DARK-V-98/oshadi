3392a7529f4c3a4082bbce7bb37dae70dc657e3b
Vishwa thimira Vidarshana 1234 <thimira.vishwa2003@gmail.com>
2025-12-20 09:55:01 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,11 +1,10 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: "AIzaSyCiggxCjWaI-AaWgkrdFhUTCiR7aNBxpM0",
-  authDomain: "cdesystem.firebaseapp.com",
-  databaseURL: "https://cdesystem-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "cdesystem",
-  storageBucket: "cdesystem.firebasestorage.app",
-  messagingSenderId: "379151186508",
-  appId: "1:379151186508:web:a73a943c291a9008de52e9"
+  apiKey: "AIzaSyAltgqFPyYvkCvErXJc9CwGTmGV6MaXelI",
+  authDomain: "propoint-ba6ks.firebaseapp.com",
+  projectId: "propoint-ba6ks",
+  storageBucket: "propoint-ba6ks.appspot.com",
+  messagingSenderId: "715106973489",
+  appId: "1:715106973489:web:91fd0d99553761de52e210"
 };
