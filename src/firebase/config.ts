1e0e1a4bbb5fbe2f0e5131917c84fb6331933e42
Vishwa thimira Vidarshana 1234 <thimira.vishwa2003@gmail.com>
2026-01-01 20:20:48 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: "",
+  apiKey: "AIzaSyDsUgFnCZts66oVbQfdPdOmrEO5nm9meGU",
   authDomain: "esystemlkapp.firebaseapp.com",
   projectId: "esystemlkapp",
   storageBucket: "esystemlkapp.firebasestorage.app",
